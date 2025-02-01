46d60c250d38195a1ca1f204958443b68ad07e46
This is Cath! <160484851+Xiuem@users.noreply.github.com>
2025-02-01 12:32:18 +0700

    Add files via upload

--- Source.lua
+++ Source.lua
@@ -135,6 +135,26 @@
         until game.Players.LocalPlayer.Team ~= nil
         --- Creating Ui ---
         
+        local Icons = {}
+        local Success, Response =
+            pcall(
+            function()
+                Icons =
+                    HttpService:JSONDecode(
+                    game:HttpGetAsync(
+                        "https://raw.githubusercontent.com/dawid-scripts/Fluent/refs/heads/master/src/Icons.lua"
+                    )
+                ).icons
+            end
+        )
+        local MMBStatus = ""
+        local CheckMobile = function()
+            if
+                game:GetService("UserInputService").TouchEnabled
+             then
+                return true 
+            end
+        end 
         IsMobile = CheckMobile()
         Size11,Size22 = 600,460
         if IsMobile then 
